35927aaaf8c8fdbe1d86b3e814f8ef7d6430c1ca
Unknownmaster0 <sagar192002singh@gmail.com>
2023-11-23 20:22:00 +0530

    morris traversal condition changes

--- morris_traversal_of_tree.cpp
+++ morris_traversal_of_tree.cpp
@@ -1,54 +1,92 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node{
-    public:
-        int data;
-        Node* left;
-        Node* right;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
 
-        Node(int val)
-        {
-            this -> data = val;
-            this -> right = NULL;
-            this -> left = NULL;
-        }
+    Node(int val)
+    {
+        this->data = val;
+        this->right = NULL;
+        this->left = NULL;
+    }
 };
 
-void MorrisTraversal(Node* root)
+Node *buildTree(Node *root)
 {
-    if(root == NULL)
-        return ;
+    // enter the data of the root
+    printf("enter data: ");
+    int num;
+    cin >> num;
+
+    root = new Node(num);
+
+    // base case :
+    // cout<<"we consider -1 as NULL. To terminate the loop."<<endl;
+    if (num == -1)
+        return NULL;
+
+    // now filling the left and right part of the tree.
+
+    // Left Part
+    cout << "enter the data to fill on left of -> " << num << endl;
+
+    // recursive call for the left part of the tree
+    root->left = buildTree(root->left);
+
+    // Right Part
+    cout << "enter the data to fill on right -> " << num << endl;
+
+    // recursive call for the left part of the tree
+    root->right = buildTree(root->right);
+
+    // finally return the root
+    return root;
+}
+
+void MorrisTraversal(Node *root)
+{
+    if (root == NULL)
+        return;
 
     // we need two iterator.
-    Node * current, * previous;
+    Node *current, *previous;
     current = root;
 
-    while(current != NULL)
+    cout<<"hello"<<endl;
+    while (current != NULL)
     {
-        if(current -> left == NULL)
+        if (current->left == NULL)
         {
-            cout<< current->data<<" ";
-            current = current -> right;
+            cout << current->data << " ";
+            current = current->right;
         }
 
-        else{
+        else
+        {
             // then find the predecessor of current.
-            previous = current -> left;
-            while(previous != NULL && previous -> right != current)
+            previous = current->left;
+            while (previous->right != NULL && previous->right != current)
+
                 previous = previous->right;
 
             // if previous right is null -: it means we need to point the predessor or previour to the current elements.
-            if(previous -> right == NULL){
-                previous -> right = current;
-                current = current -> left;
+            if (previous->right == NULL)
+            {
+                previous->right = current;
+                current = current->left;
             }
 
             // if the previous right is already pointing to the current element -: then simply remove the made link
-            else{
-                previous -> right = NULL;
-                cout<<current->data<<" ";
-                current = current -> right;
+            else
+            {
+                previous->right = NULL;
+                cout << current->data << " ";
+                current = current->right;
             }
         }
     }
@@ -56,6 +94,10 @@ void MorrisTraversal(Node* root)
 
 int main()
 {
-    
+    Node *root = NULL;
+    root = buildTree(root);
+    cout << "root -> data -> " << root->data << endl;
+    MorrisTraversal(root);
+// 5 4 -1 -1 3 -1 -1
     return 0;
 }
